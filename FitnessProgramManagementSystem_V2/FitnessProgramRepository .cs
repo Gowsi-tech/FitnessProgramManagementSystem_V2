debca6fb31965c388a2315c3ea636b99b4d4015a
UT02000 <gowsig926@gmail.com>
2024-10-11 12:59:52 +0530

    Q11,12,13

---
+++ FitnessProgramManagementSystem_V2/FitnessProgramRepository .cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgramRepository
+    {
+        string ConnectionString = "server=(localdb)\\MSSQLLocalDB; Database=FitnessProgramManagement; Trusted_connection= True; TrustServerCertificate=True";
+
+        public void createprogram(int FitnessProgramId, string Title, string Duration, decimal Price)
+        {
+            string insertQuery = @"
+                        INSERT INTO FitnessPrograms(FitnessProgramId, Title, Duration, Price) VALUES (@FitnessProgramId,@Title,@Duration, @Price)";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(insertQuery, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+    }
+}
--- FitnessProgramManagementSystem_V2/Program.cs
+++ FitnessProgramManagementSystem_V2/Program.cs
@@ -10,6 +10,23 @@ namespace FitnessProgramManagementSystem_V2
     {
         static void Main(string[] args)
         {
+            string masterDbConnectionString = "server=(localdb)\\MSSQLLocalDB; Database=master; Trusted_connection= True; TrustServerCertificate=True";
+            string fitnessDbConnectionString = "server=(localdb)\\MSSQLLocalDB; Database=FitnessProgramManagement; Trusted_connection= True; TrustServerCertificate=True";
+
+            string databaseQuery = @"
+                            IF NOT EXISTS(SELECT * FROM master
+                            CREATE DATABASE FitnessProgramManagement)";
+
+            string tableQuery = @"
+                        IF NOT EXISTS(SELECT * FORM MASTRE
+                            CREATE TABLE FitnessPrograms 
+                            	FitnessProgramId INT IDENTITY (1,1) PRIMARY KEY
+                                Title nvarchar(50) NOT NULL
+                                 Duration nvarchar(50) NOT NULL
+                                 Price DECIMAL(10,2) )";
+
+            string insertQuery = @"
+                SELECT * FROM FitnessPrograms(FitnessProgramId,Title,Duration,Price) VALUES (' weight Training' , '6 months',  10 ) ";
         }
     }
 }
